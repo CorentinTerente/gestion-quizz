a6ea04ac435469e20377feb43bda7cd659cebe5f
Corentin <corentin.terente@gmail.com>
2018-11-14 15:32:19 +0100

    initialisation du projet

---
+++ src/fr/diginamic/console/QuizzAdminConsoleApp.java
@@ -0,0 +1,8 @@
+package fr.diginamic.console;
+
+public class QuizzAdminConsoleApp {
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/fr/diginamic/model/Question/Question.java
@@ -0,0 +1,5 @@
+package fr.diginamic.model.Question;
+
+public class Question {
+
+}
