b19d03c5fb5a19ab765413d30659aa13deb44898
Corentin <corentin.terente@gmail.com>
2018-11-15 09:51:38 +0100

    ajout de l'execution du quizz

--- src/fr/diginamic/console/QuizzAdminConsoleApp.java
+++ src/fr/diginamic/console/QuizzAdminConsoleApp.java
@@ -11,6 +11,7 @@ public class QuizzAdminConsoleApp {
 		int choixUser = 0;
 		boolean quizz = true;
 		List<Question> listeQuestions = new ArrayList<Question>();
+		int nbPoints=0;
 		
 		while(quizz) {
 			System.out.println("veuillez entrez 1 , 2 , 3 , 4 ou 99");
@@ -19,7 +20,7 @@ public class QuizzAdminConsoleApp {
 			switch(choixUser) {
 			//liste les questions dans la liste
 				case 1: System.out.println("Liste questions");
-					afficherQuestion(listeQuestions);
+					afficherTouteQuestion(listeQuestions);
 			break;
 			//ajoute une question avec le nombre de proposition choisi 
 				case 2: System.out.println("Ajout d’une nouvelle question");
@@ -31,7 +32,14 @@ public class QuizzAdminConsoleApp {
 			break;
 			//execute le quizz
 				case 4: System.out.println("Exécution du quizz");
-					
+					for(Question laQuestion : listeQuestions) {
+						System.out.println((listeQuestions.indexOf(laQuestion)+1)+")  "+laQuestion.getIntitule());
+						afficherUneQuestion(laQuestion);
+						System.out.println("votre reponse ?");
+						String reponse = questionUser.nextLine();
+						nbPoints+=repondreQuestion(reponse,laQuestion);
+					}
+					System.out.println("Vos Points : "+nbPoints);
 			break;
 				case 99: System.out.println("Au revoir");
 				quizz= false;
@@ -42,14 +50,13 @@ public class QuizzAdminConsoleApp {
 		questionUser.close();
 	}
 	
-	public static void afficherQuestion(List<Question> listeQuestions) {
+	public static void afficherTouteQuestion(List<Question> listeQuestions) {
 		for (Question question : listeQuestions) {
 			System.out.println((listeQuestions.indexOf(question)+1)+")  "+question.getIntitule());
-			for(String proposition : question.getPropositions()) {
-				System.out.println("	-"+proposition);
-			}
+			afficherUneQuestion(question);
 		}
 	}
+	
 	
 	public static void ajouterQuestion(Scanner sc,List<Question> listeQuestions) {
 		System.out.println("veuillez saisir l'intitule de la question");
@@ -72,6 +79,20 @@ public class QuizzAdminConsoleApp {
 		int numQuestions = Integer.parseInt(sc.nextLine());
 		if(!listeQuestion.isEmpty()) {
 			listeQuestion.remove(numQuestions);
+		}
+	}
+	
+	public static int repondreQuestion(String reponse,Question laQuestion) {
+		int point = 0;
+		if(laQuestion.verifierReponse(reponse)) {
+			point = 1;
+		}
+		return point;
+	}
+	
+	public static void afficherUneQuestion(Question uneQuestion) {
+		for(String proposition : uneQuestion.getPropositions()) {
+			System.out.println("	-"+proposition);
 		}
 	}
 
